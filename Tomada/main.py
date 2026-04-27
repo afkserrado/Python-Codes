d574ee0a5886dc119d333bc4b200abfcac7ae54c
Anderson Serrado <akserrado@gmail.com>
2026-04-27 14:39:35 -0300

    Ajuste do caminho do arquivo de log

--- Tomada/main.py
+++ Tomada/main.py
@@ -1,11 +1,14 @@
 import asyncio
 import logging
+import os
 from controller import Controller
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 logging.basicConfig(
-    filename="tapo.log",
+    filename=os.path.join(BASE_DIR, "tapo.log"),
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    format="%(asctime)s - %(message)s",
     force=True
 )
 
